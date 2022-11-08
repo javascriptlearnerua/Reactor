de45624d213d554a674467dd0a642f34f1c0c868
itstudentua <itstudentua@gmail.com>
2022-11-08 16:42:58 +0200

    JSX syntax; variables write with {}

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-ReactDOM.render(
-    <h1>Hello</h1>,
-    document.getElementById("app")
+const inputClick = () => alert('clicked');
+const mouseOver = () => console.log('mouse over');
+const helpText = "Help text";
+
+const inp = (
+    <div className="main-section">
+        <h1>Hello React!</h1>
+        <input 
+            placeholder={helpText}
+            onClick={inputClick} 
+            onMouseEnter={mouseOver}
+        />
+    </div>
 );
+
+const app = document.getElementById("app");
+
+
+ReactDOM.render(inp, app);
